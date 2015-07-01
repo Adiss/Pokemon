f3ffc7b96fc93d515eec64c346e2fec4c8f07845
Jakab Adam <adiss.b17@gmail.com>
2015-07-01 13:08:21 +0200

    Battle test

--- src/main/java/hu/experiment_team/Main.java
+++ src/main/java/hu/experiment_team/Main.java
@@ -3,124 +3,72 @@
  */
 package hu.experiment_team;
 
+import hu.experiment_team.dao.MoveDaoJDBC;
+import hu.experiment_team.dao.PokemonDaoJDBC;
+import hu.experiment_team.dao.TrainerDaoJDBC;
+import hu.experiment_team.models.Pokemon;
 import hu.experiment_team.models.Trainer;
-import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-import javafx.stage.Stage;
-
+;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * Main class of the application.
- * @author Jakab �d�m
+ * @author Jakab Ádám
  * */
-public class Main extends Application {
+public class Main {
 
-    /**
-     * This method will be called when the program start, launches the JavaFX application.
-     * @param args Arguments of the program
-     * */
     public static void main(String[] args){
-        launch(args);
-    }
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("Pokemon");
-
-        // RegistrationScene BEGIN
-
-        VBox registrationRoot = new VBox();
-        registrationRoot.setSpacing(5);
-        registrationRoot.setPadding(new Insets(10, 10, 10, 10));
-
-        Label registrationNameLabel = new Label("Username");
-        Label registrationDisplayNameLabel = new Label("Display name");
-        Label registrationPasswordLabel = new Label("Password");
-        Label registrationPassword2Label = new Label("Password Again");
-        Label registrationEmailLabel = new Label("E-mail");
-        TextField registrationNameTextField = new TextField();
-        TextField registrationDisplayNameTextField = new TextField();
-        TextField registrationPasswordTextField = new TextField();
-        TextField registrationPassword2TextField = new TextField();
-        TextField registrationEmailTextField = new TextField();
-
-        Button registrationSubmit = new Button("Submit");
-
-        registrationRoot.getChildren().addAll(
-                registrationNameLabel,
-                registrationNameTextField,
-                registrationDisplayNameLabel,
-                registrationDisplayNameTextField,
-                registrationPasswordLabel,
-                registrationPasswordTextField,
-                registrationPassword2Label,
-                registrationPassword2TextField,
-                registrationEmailLabel,
-                registrationEmailTextField,
-                registrationSubmit);
-
-        Scene registrationScene = new Scene(registrationRoot, 500, 500, Color.WHITE);
-
-        // RegistrationScene END
-
-        // LoginScene BEGIN
-
-        VBox loginRoot = new VBox();
-        loginRoot.setSpacing(5);
-        loginRoot.setPadding(new Insets(10, 10, 10, 10));
-
-        Label loginNameLabel = new Label("Username");
-        Label loginPasswordLabel = new Label("Password");
-        Label loginErrorField = new Label("");
-        TextField loginNameTextField = new TextField();
-        TextField loginPasswordTextField = new TextField();
-
-        Button loginButton = new Button("Login");
-        Button registerButton = new Button("Registration");
-        HBox buttonHolder = new HBox();
-        buttonHolder.setSpacing(10);
-        buttonHolder.setPadding(new Insets(20, 20, 20, 20));
-        buttonHolder.getChildren().addAll(loginButton, registerButton);
-
-        loginButton.setOnAction(e -> {
-            Trainer user = UserMethods.INSTANCE.login(loginNameTextField.getText(), loginPasswordTextField.getText());
-            if((user != null) && (user.getOnline() == 1)){
-                loginErrorField.setText("Sikeres Bejelentkezés!");
-            } else
-                loginErrorField.setText("Hibás felhasználónév vagy jelszó!");
-        });
-        registerButton.setOnAction(e -> {
-            primaryStage.setScene(registrationScene);
-        });
-
-        loginRoot.getChildren().addAll(loginErrorField ,loginNameLabel, loginNameTextField, loginPasswordLabel, loginPasswordTextField, buttonHolder);
-
-        Scene loginScene = new Scene(loginRoot, 500, 300, Color.WHITE);
-        primaryStage.setScene(loginScene);
-
-        registrationSubmit.setOnAction(e -> {
-            List<String> errors = UserMethods.INSTANCE.register(registrationNameTextField.getText(), registrationDisplayNameTextField.getText(), registrationPasswordTextField.getText(), registrationPassword2TextField.getText(), registrationEmailTextField.getText());
-            if (errors.get(0).equals("Sikeres regisztracio!"))
-                primaryStage.setScene(loginScene);
-            else
-                for (String err : errors)
-                    registrationRoot.getChildren().add(new Label(err));
-        });
-
-        // LoginScene END
+        // A trainer belép a rendszerbe
+        Trainer t = UserMethods.INSTANCE.login("adiss", "422341");
+        List<Pokemon> ownedPokemons = PokemonDaoJDBC.INSTANCE.getOwnedPokemons(t.getId());
 
 
+        // Kap 6 random pokémont
+        if(ownedPokemons.size() < 6){
+            PokemonDaoJDBC.INSTANCE.addOwnedPokemon(t.getId(), PokemonDaoJDBC.INSTANCE.getRandomPokemon().getId());
+            PokemonDaoJDBC.INSTANCE.addOwnedPokemon(t.getId(), PokemonDaoJDBC.INSTANCE.getRandomPokemon().getId());
+            PokemonDaoJDBC.INSTANCE.addOwnedPokemon(t.getId(), PokemonDaoJDBC.INSTANCE.getRandomPokemon().getId());
+            PokemonDaoJDBC.INSTANCE.addOwnedPokemon(t.getId(), PokemonDaoJDBC.INSTANCE.getRandomPokemon().getId());
+            PokemonDaoJDBC.INSTANCE.addOwnedPokemon(t.getId(), PokemonDaoJDBC.INSTANCE.getRandomPokemon().getId());
+            PokemonDaoJDBC.INSTANCE.addOwnedPokemon(t.getId(), PokemonDaoJDBC.INSTANCE.getRandomPokemon().getId());
+        }
 
-        primaryStage.setResizable(false);
-        primaryStage.show();
+        // Az első pokemon megüti a 2. pokemont
 
+        System.out.println("Elso pokemon utes elott: ");
+        System.out.println(ownedPokemons.get(1).getName());
+        System.out.println(ownedPokemons.get(1).getHp());
+        System.out.println();
+
+        System.out.println("Masodik pokemon utes elott: ");
+        System.out.println(ownedPokemons.get(0).getName());
+        System.out.println(ownedPokemons.get(0).getHp());
+        System.out.println();
+
+        Battle.INSTANCE.dealDamage(ownedPokemons.get(0), ownedPokemons.get(1), MoveDaoJDBC.INSTANCE.getMoveById(ownedPokemons.get(0).getMove1Id()));
+
+        /*
+
+         Scanner scanIn = new Scanner(System.in);
+
+        System.out.println("Enter your username:");
+        String username = scanIn.nextLine();
+        System.out.println("Enter your password:");
+        String password = scanIn.nextLine();
+
+        Trainer user = UserMethods.INSTANCE.login(username, password);
+
+        if(user != null){
+            System.out.println("Logged in!");
+        } else {
+            System.out.println("Wrong username or password!");
+        }
+
+        scanIn.close();
+
+        */
     }
 }
